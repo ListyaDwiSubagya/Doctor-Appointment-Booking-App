fdcb3d650addad79fdfad0091d6866615c9183aa
ListyaDwiSubagya <listyodwi062@gmail.com>
2024-11-07 18:36:35 +0700

    update ui

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+
+      <div className='text-center text-2xl text-gray-500'>
+        <p>ABOUT <span className='text-gray-700 font-medium'>US</span></p>
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-12'>
+        <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium obcaecati eaque illo reprehenderit accusamus quibusdam nostrum exercitationem minus dicta iste!</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore, voluptatem quae? Et expedita aliquam tempore eos itaque suscipit eius nulla! Vero officiis nihil repellat nobis recusandae dicta sunt quibusdam reprehenderit?</p>
+          <b className='text-gray-600'>Lorem</b>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil hic cupiditate, sed quod eligendi voluptate repellendus provident velit dolor magnam esse minus suscipit, nam corrupti!</p>
+        </div>
+      </div>
+
+      <div className='text-xl my-4'>
+        <p>WHY <span className='text-gray-700 font-semibold'>CHOOSE US</span></p>
+      </div>
+
+      <div className='flex flex-col md:flex-row mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Efficiency:</b>
+          <p>Streamlined appointment scheduling that fits into your busy lifestyle.</p>
+        </div>
+
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Convencience:</b>
+          <p>Acces to a network of trusted healthcare professionals in your area.</p>
+        </div>
+
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Personalization:</b>
+          <p>Tailored recommendations and reminders to help you stay on top of your health.</p>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
